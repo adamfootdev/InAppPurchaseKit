43a69886fdc376e75959e6c0caa043a7dc013bcd
Adam Foot <admin@adamfoot.dev>
2025-06-10 21:20:03 +0100

    Updated app icon view

--- Sources/InAppPurchaseKit/Views/AppIconView.swift
+++ Sources/InAppPurchaseKit/Views/AppIconView.swift
@@ -20,37 +20,9 @@ struct AppIconView: View {
             bundle: .main
         )
         .resizable()
-        .scaledToFill()
-        #if os(iOS) || os(tvOS)
-        .clipShape(RoundedRectangle(
-            cornerRadius: imageCornerRadius,
-            style: .continuous
-        ))
-        #elseif os(visionOS) || os(watchOS)
-        .clipShape(Circle())
-        #endif
+        .scaledToFit()
         .frame(width: imageWidth, height: imageHeight)
-        .overlay {
-            #if os(iOS) || os(tvOS)
-            RoundedRectangle(
-                cornerRadius: imageCornerRadius,
-                style: .continuous
-            )
-            .stroke(Color.appIconBorder, lineWidth: 1)
-            #elseif os(visionOS) || os(watchOS)
-            Circle()
-                .stroke(Color.appIconBorder, lineWidth: 1)
-            #endif
-        }
         .accessibilityHidden(true)
-    }
-
-    private var imageCornerRadius: CGFloat {
-        #if os(tvOS)
-        return 32
-        #else
-        return 16
-        #endif
     }
 
     private var imageWidth: CGFloat {
